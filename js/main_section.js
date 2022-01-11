23881953b78979fe0567a6ac9c6b2e085ba25de0
yuziwoo <yuziwoo@naver.com>
2022-01-11 10:38:03 +0900

    section_03 반응형 및 리버스 효과 완료

--- js/main_section.js
+++ js/main_section.js
@@ -74,7 +74,7 @@ window.addEventListener("DOMContentLoaded", (e) => {
     }
 
     if(pagey > rect.y){
-      if(window.innerWidth >= 800 && rect.y < -section_01.offsetHeight-section_02.offsetHeight){
+      if(window.innerWidth >= 560 && rect.y < -section_01.offsetHeight-section_02.offsetHeight){
         section_03_height = - rect.y - section_01.offsetHeight - section_02.offsetHeight;
         if(section_03_height >= 150){
           if(!section_03_event[0]){
@@ -344,6 +344,291 @@ window.addEventListener("DOMContentLoaded", (e) => {
         }
 
       }
+    }/*(pagey > rect.y)*/
+
+    if(pagey < rect.y){
+      if(window.innerWidth >= 560 && rect.y < -section_01.offsetHeight-section_02.offsetHeight){
+        section_03_height = - rect.y - section_01.offsetHeight - section_02.offsetHeight;
+        if(section_03_height < 150){
+          if(section_03_event[0]){
+            section_03_img1.style.opacity = 0.4;
+            section_03_event.splice(0, 1, false);
+          }
+        }
+        if(section_03_height < 200){
+          if(section_03_event[1]){
+            section_03_img1.style.opacity = 0.5;
+            section_03_event[1] = false;
+          }
+        }
+        if(section_03_height < 250){
+          if(section_03_event[2]){
+            section_03_img1.style.opacity = 0.6;
+            section_03_event[2] = false;
+          }
+        }
+        if(section_03_height < 300){
+          if(section_03_event[3]){
+            section_03_img1.style.opacity = 0.7;
+            section_03_event[3] = false;
+          }
+        }
+        if(section_03_height < 350){
+          if(section_03_event[4]){
+            section_03_img1.style.opacity = 0.8;
+            section_03_event[4] = false;
+          }
+        }
+        if(section_03_height < 400){
+          if(section_03_event[5]){
+            section_03_img1.style.opacity = 0.9;
+            section_03_event[5] = false;
+          }
+        }
+        if(section_03_height < 650){
+          if(section_03_event[6]){
+            section_03_img1.style.opacity = 1;
+            section_03_headline_wrapper.style.opacity = 0;
+            section_03_event[6] = false;
+          }
+        }
+        if(section_03_height < 700){
+          if(section_03_event[7]){
+            section_03_headline_wrapper.style.opacity = 0.3;
+            section_03_event[7] = false;
+          }
+        }
+        if(section_03_height < 750){
+          if(section_03_event[8]){
+            section_03_headline_wrapper.style.opacity = 0.6;
+            section_03_headline1.style.transform = "none";
+            section_03_event[8] = false;
+          }
+        }
+        if(section_03_height < 800){
+          if(section_03_event[9]){
+            section_03_headline_wrapper.style.opacity = 0.8;
+            section_03_headline1.style.transform = "scale(0.8)";
+            section_03_event[9] = false;
+          }
+        }
+        if(section_03_height < 850){
+          if(section_03_event[10]){
+            section_03_headline_wrapper.style.opacity = 1;
+            section_03_headline1.style.transform = "scale(0.7)";
+            section_03_headline2.style.transform = "none";
+            section_03_event[10] = false;
+          }
+        }
+        if(section_03_height < 900){
+          if(section_03_event[11]){
+            section_03_headline1.style.transform = "scale(0.6)";
+            section_03_headline2.style.transform = "scale(0.6)";
+            section_03_headline1.style.opacity = 1;
+            section_03_headline2.style.opacity = 0;
+            section_03_event[11] = false;
+          }
+        }
+        if(section_03_height < 950){
+          if(section_03_event[12]){
+            section_03_headline1.style.transform = "scale(0.5)";
+            section_03_headline2.style.transform = "scale(0.5)";
+            section_03_headline1.style.opacity = 0;
+            section_03_headline2.style.opacity = 1;
+            section_03_event[12] = false;
+          }
+        }
+        if(section_03_height < 1000){
+          if(section_03_event[13]){
+            section_03_headline1.style.transform = "scale(0.4)";
+            section_03_headline2.style.transform = "scale(0.4)";
+            section_03_img1.style.backgroundImage = `url("img/gradient1.jpg")`;
+            section_03_img1.style.backgroundRepeat = "no-repeat";
+            section_03_img1.style.backgroundSize = "cover";
+            section_03_img1.style.backgroundPosition = "center";
+            section_03_event[13] = true;
+          }
+        }
+        if(section_03_height < 1050){
+          if(section_03_event[14]){
+            section_03_headline1.style.transform = "scale(0.35)";
+            section_03_headline2.style.transform = "scale(0.35)";
+            section_03_event[14] = false;
+          }
+        }
+        if(section_03_height < 1100){
+          if(section_03_event[15]){
+            section_03_headline1.style.transform = "scale(0.3)";
+            section_03_headline2.style.transform = "scale(0.3)";
+            section_03_event[15] = false;
+          }
+        }
+        if(section_03_height < 1150){
+          if(section_03_event[16]){
+            section_03_headline1.style.transform = "scale(0.25)";
+            section_03_headline2.style.transform = "scale(0.25)";
+            section_03_event[16] = false;
+          }
+        }
+        if(section_03_height < 1200){
+          if(section_03_event[17]){
+            section_03_headline1.style.transform = "scale(0.22)";
+            section_03_headline2.style.transform = "scale(0.22)";
+            section_03_event[17] = false;
+          }
+        }
+        if(section_03_height < 1250){
+          if(section_03_event[18]){
+            section_03_headline1.style.transform = "scale(0.2)";
+            section_03_headline2.style.transform = "scale(0.2)";
+            section_03_event[18] = false;
+          }
+        }
+        if(section_03_height < 1500){
+          if(section_03_event[19]){
+            section_03_headline1.style.transform = "scale(0.18)";
+            section_03_headline2.style.transform = "scale(0.18)";
+            section_03_middle.style.opacity = 0;
+            section_03_middle.style.transform = "none";
+            section_03_event[19] = false;
+          }
+        }
+        if(section_03_height < 1550){
+          if(section_03_event[20]){
+            section_03_middle.style.opacity = "0.3";
+            section_03_middle.style.transform = "scale(0.9)";
+            section_03_event[20] = false;
+          }
+        }
+        if(section_03_height < 1600){
+          if(section_03_event[21]){
+            section_03_middle.style.opacity = "0.5";
+            section_03_middle.style.transform = "scale(0.8)";
+            section_03_event[21] = false;
+          }
+        }
+        if(section_03_height < 1650){
+          if(section_03_event[22]){
+            section_03_middle.style.opacity = "0.7";
+            section_03_middle.style.transform = "scale(0.7)";
+            section_03_event[22] = false;
+          }
+        }
+        if(section_03_height < 1700){
+          if(section_03_event[23]){
+            section_03_middle.style.opacity = "0.9";
+            section_03_middle.style.transform = "scale(0.6)";
+            section_03_event[23] = false;
+          }
+        }
+        if(section_03_height < 1750){
+          if(section_03_event[24]){
+            section_03_middle.style.opacity = "1";
+            section_03_middle.style.transform = "scale(0.5)";
+            section_03_event[24] = false;
+          }
+        }
+        if(section_03_height < 1800){
+          if(section_03_event[25]){
+            section_03_middle.style.transform = "scale(0.4)";
+            section_03_event[25] = false;
+          }
+        }
+        if(section_03_height < 1850){
+          if(section_03_event[26]){
+            section_03_middle.style.transform = "scale(0.3)";
+            section_03_event[26] = false;
+          }
+        }
+        if(section_03_height < 1900){
+          if(section_03_event[27]){
+            section_03_middle.style.transform = "scale(0.25)";
+            section_03_event[27] = false;
+          }
+        }
+        if(section_03_height < 1950){
+          if(section_03_event[28]){
+            section_03_middle.style.transform = "scale(0.22)";
+            section_03_event[28] = false;
+          }
+        }
+        if(section_03_height < 2000){
+          if(section_03_event[29]){
+            section_03_middle.style.transform = "scale(0.2)";
+            section_03_event[29] = false;
+          }
+        }
+        if(section_03_height < 2050){
+          if(section_03_event[30]){
+            section_03_middle.style.transform = "scale(0.18)";
+            section_03_event[30] = false;
+          }
+        }
+        if(section_03_height < 2100){
+          if(section_03_event[31]){
+            section_03_middle.style.transform = "scale(0.15)";
+            section_03_event[31] = false;
+          }
+        }
+        if(section_03_height < 2450){
+          if(section_03_event[32]){
+            section_03_middle.style.transform = "scale(0.12)";
+            section_03_text.style.opacity = 0;
+            section_03_event[32] = false;
+          }
+        }
+        if(section_03_height < 2950){
+          if(section_03_event[33]){
+            section_03_text.style.opacity = 1;
+            section_03_headline_wrapper.style.opacity = 1;
+            section_03_event[33] = false;
+          }
+        }
+        if(section_03_height < 3000){
+          if(section_03_event[34]){
+            section_03_headline_wrapper.style.opacity = 0.7;
+            section_03_event[34] = false;
+          }
+        }
+        if(section_03_height < 3050){
+          if(section_03_event[35]){
+            section_03_headline_wrapper.style.opacity = 0.6;
+            section_03_event[35] = false;
+          }
+        }
+        if(section_03_height < 3100){
+          if(section_03_event[36]){
+            section_03_headline_wrapper.style.opacity = 0.5;
+            section_03_event[36] = false;
+          }
+        }
+        if(section_03_height < 3150){
+          if(section_03_event[37]){
+            section_03_headline_wrapper.style.opacity = 0.4;
+            section_03_event[37] = false;
+          }
+        }
+        if(section_03_height < 3200){
+          if(section_03_event[38]){
+            section_03_headline_wrapper.style.opacity = 0.3;
+            section_03_event[38] = false;
+          }
+        }
+        if(section_03_height < 3250){
+          if(section_03_event[39]){
+            section_03_headline_wrapper.style.opacity = 0.2;
+            section_03_event[39] = false;
+          }
+        }
+        if(section_03_height < 3300){
+          if(section_03_event[40]){
+            section_03_headline_wrapper.style.opacity = 0.1;
+            section_03_event[40] = false;
+          }
+        }
+
+      }
+      console.log(section_03_height);
     }/*(pagey > rect.y)*/
 
     pagey = rect.y;
