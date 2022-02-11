425ad079731f85d3f136802f20934150a46cfeac
yuziwoo <yuziwoo@naver.com>
2022-02-11 16:03:20 +0900

    works 수정

--- js/works.js
+++ js/works.js
@@ -1,71 +1,53 @@
 window.addEventListener("DOMContentLoaded", (e) => {
-  let click_count = 0;
-  let background_image = document.getElementsByClassName("background_image")[0];
-  let content_box = document.getElementsByClassName("content_box")[0];
-  let content_list = document.getElementsByClassName("content_list")[0];
-  let content = document.getElementsByClassName("content");
-  let button_up = document.getElementsByClassName("button_up")[0];
-  let button_down = document.getElementsByClassName("button_down")[0];
-  let info = document.getElementsByClassName("info")[0];
-  let video = document.getElementsByTagName("video")[0];
-  let button_box = document.getElementsByClassName("button_box")[0];
-  let size = 0;
+  let main = document.getElementsByClassName("main_content")[0];
+  let img = document.getElementsByClassName("works_img");
+  let top = document.documentElement.scrollTop;
+  let img_top = [];
+  let scale = [];
 
-  content_list.addEventListener("scroll", () => {
-    console.log(content_list.scrollTop);
-  })
+  let intro_content = document.getElementsByClassName("intro_content");
 
-  let content_values = [
-    ["baobab", "https://yuziwoo.github.io/baobab2/"],
-    ["elgroup", "elgroup/index.html"]
-  ]
-
-  content_list.addEventListener("click", () => {
-    if(click_count < 1){
-      content_box.classList.add("content_box_on");
-      info.classList.add("info_on");
-    }
-  });
-
-  for(let i=0; i<content.length; i++) {
-    content[i].addEventListener("click", (e) => {
-      for(let j=0; j<content.length; j++) {
-        if(i != j){
-          content[j].classList.remove("active");
-        }
-      }//반복문 J 종료
-
-      e.target.parentElement.classList.add("active");
-      content_list.scrollTo(0, e.target.parentElement.offsetTop - 253);
-
-      background_image.src = `img/${content_values[i][0]}.jpg`;
-      video.src = `video/${content_values[i][0]}.mp4`;
-      button_box.href = content_values[i][1];
-    })
+  for (let i=0; i<img.length; i++) {
+    img_top[i] = img[i].offsetTop + main.offsetTop;
   }
 
-  button_up.addEventListener("click", () => {
-    content_list.scrollTo(0, 200);
-  })
-  button_down.addEventListener("click", () => {
-    content_list.scrollTo(0, 600);
-  })
-
-  if(size < 1){
-    if(window.innerWidth <= 550){
-      alert("해당 페이지는 500px 이상의 디스플레이 사용을 권장합니다.")
-      size++
-    }
-  }
-
-  window.addEventListener("resize", (e) => {
-    if(size < 1){
-      if(window.innerWidth <= 550){
-        alert("해당 페이지는 500px 이상의 디스플레이 사용을 권장합니다.")
-        size++
+  for (let i=0; i<intro_content.length; i++) {
+    if (intro_content[i].offsetTop - 150 < top + window.innerHeight) {
+      if (!intro_content[i].classList.contains("intro_content_on")) {
+        intro_content[i].classList.add("intro_content_on")
       }
     }
+  }
+
+  console.log (intro_content[2].offsetTop)
+
+  window.addEventListener("resize", (e) => {
+    for (let i=0; i<img.length; i++) {
+      img_top[i] = img[i].offsetTop + main.offsetTop;
+    }
   })
 
-  
+  window.addEventListener("scroll", (e) => {
+    top = document.documentElement.scrollTop;
+
+    for (let i=0; i<img.length; i++) {
+      if (img_top[i] >= top - 300 ) {
+        scale[i] = (img_top[i] - 300 - top) / 10000 * 3 + 1;
+        if (scale[i] <= 1.3) {
+          if (scale[i] < 1) {
+            scale[i] = 1;
+          }
+          img[i].style.transform = `scale(${scale[i]})`;
+        }
+      }
+    }
+
+     for (let i=0; i<intro_content.length; i++) {
+       if (intro_content[i].offsetTop - 150 < top + window.innerHeight) {
+         if (!intro_content[i].classList.contains("intro_content_on")) {
+           intro_content[i].classList.add("intro_content_on")
+         }
+       }
+     }
+  })
 });
